44a52da049241ba4e1ba30f6654c3bf12466a35e
Suk M <Suk_91@outlook.com>
2018-09-26 12:30:04 -0400

    プロジェクト ファイルを追加します。

---
+++ SimulatorApp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SimulatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BonusID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Slot Engine DLL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.DLLButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "PayTable XML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.PayTableButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(659, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Start Game";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.StartGameButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BonusID});
+            this.dataGridView1.Location = new System.Drawing.Point(72, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(476, 124);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // BonusID
+            // 
+            this.BonusID.HeaderText = "BonusID";
+            this.BonusID.Name = "BonusID";
+            this.BonusID.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Slot Simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BonusID;
+    }
+}
+
---
+++ SimulatorApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameSimulator;
+using System.Reflection;
+
+namespace SimulatorApp
+{
+    public partial class Form1 : Form
+    {
+        private GameSimulator.GameSimulator m_GameSimulator;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+           // m_GameSimulator = new GameSimulator.GameSimulator();
+        }
+
+
+        private void DLLButton_Click(object sender, EventArgs e)
+        {
+            string sFilePath;
+            OpenFileDialog openfileDialog = new OpenFileDialog();
+            if (openfileDialog.ShowDialog() == DialogResult.OK)
+            {
+                sFilePath = openfileDialog.FileName;
+
+                var dll = Assembly.LoadFile(sFilePath);
+                string sName = dll.GetName().Name;
+                Type type = dll.GetType("GameSimulator." + sName);
+                m_GameSimulator = (GameSimulator.GameSimulator)Activator.CreateInstance(type);//(simType)m_GameSimulator;
+                m_GameSimulator.Initialize();
+                //this.dataGridView1
+            }
+        }
+
+        private void PayTableButton_Click(object sender, EventArgs e)
+        {
+            string sFilePath;
+            OpenFileDialog openfileDialog = new OpenFileDialog();
+            if (openfileDialog.ShowDialog() == DialogResult.OK)
+            {
+                sFilePath = openfileDialog.FileName;
+                m_GameSimulator.LoadXML(sFilePath);
+            }
+        }
+
+        private void StartGameButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
